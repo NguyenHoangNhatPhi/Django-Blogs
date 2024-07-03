9c18a2f46230a901d281380900d786bde96adbdb
phi.nguyen <phi.nguyen@niteco.se>
2024-07-03 14:33:52 +0700

    Upload avatar file

--- a_users/urls.py
+++ a_users/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from a_users import views
 
 urlpatterns = [
-    path("profile/", views.profile_view, name="profile")
+    path("profile/", views.profile_view, name="profile"),
+    path("profile-edit/", views.profile_edit_view, name="profile-edit"),
 ]
--- a_users/views.py
+++ a_users/views.py
@@ -1,9 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
 from a_users.models import Profile
+from a_users.forms import ProfileForm
 
 
+@login_required
 def profile_view(request):
     profile = request.user.profile
 
     return render(request, "a_users/profile.html", {"profile": profile})
+
+
+def profile_edit_view(request):
+    form = ProfileForm(instance=request.user.profile)
+
+    if request.method == "POST":
+        form = ProfileForm(request.POST, request.FILES, instance=request.user.profile)
+
+        if form.is_valid():
+            form.save()
+            return redirect("profile")
+
+    return render(request, "a_users/profile_edit.html", {"form": form})
